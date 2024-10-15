8f560ccedb7aa7676bf178cfd96ea9fab884943e
John Yoon <fedelejohn7008@gmail.com>
2024-10-14 23:31:02 -0600

    edit license

---
+++ src/main.py
@@ -0,0 +1,12 @@
+"""
+Author: John Yoon
+Email: fedelejohn7008@gmail.com
+Version: 2.1.0
+
+This is a simple Youtube video downloading GUI application using pyside6 and yt-dlp.
+
+Copyright (c) 2024 John Yoon. All rights reserved.
+Licensed under the MIT License. See LICENSE file in the project root for more information.
+"""
+
+
